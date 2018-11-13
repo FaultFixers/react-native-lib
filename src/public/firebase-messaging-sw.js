79e88d24655c89e694ff74564293a626b12daea3
Amy Boyd <amy@amyboyd.co.uk>
2018-11-13 12:17:55 +0000

    Fix FCM messages not working.

--- src/public/firebase-messaging-sw.js
+++ src/public/firebase-messaging-sw.js
@@ -4,3 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 firebase.initializeApp({
     messagingSenderId: 'BOtUJY5HMPDfKSJDRvpzz7KgI4a5bjV6Ds-Lrxci8HstW05_NDWsndxCArgEZ-z3Xs08zg3PfaPUnugOmNw4llc',
 });
+
+// This call is required, even if messaging is unused.
+const messaging = firebase.messaging();
