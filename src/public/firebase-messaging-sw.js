f6af70b275f995e601a9cb13dc5f6d4170b40db0
Amy Boyd <amy@amyboyd.co.uk>
2018-11-13 11:29:41 +0000

    Fill in basic parts of firebase-messaging-sw.js.

---
+++ src/public/firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: 'BOtUJY5HMPDfKSJDRvpzz7KgI4a5bjV6Ds-Lrxci8HstW05_NDWsndxCArgEZ-z3Xs08zg3PfaPUnugOmNw4llc',
+});
