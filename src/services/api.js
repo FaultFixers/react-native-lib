2bac6400d51ff13c3c92c139c6558f7d8fa8ac91
Amy Boyd <amy@amyboyd.co.uk>
2019-01-21 11:16:47 +0000

    Upgrade to API v8.

--- src/services/api.js
+++ src/services/api.js
@@ -14,7 +14,7 @@ function doRequest(authorization, method, path, bodyData = null) {
             path: path,
             method: method,
             headers: {
-                'Accept': 'application/vnd.faultfixers.v7+json',
+                'Accept': 'application/vnd.faultfixers.v8+json',
                 'User-Agent': 'FaultFixers Reporting Websites',
             },
         };
